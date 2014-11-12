08c9eed41ad25281afb6e93963d2ff9f8b226f88
tunnckoCore <mamemto_100@mail.bg>
2014-11-12 04:13:07 +0200

    add linkify option and tests

--- test.js
+++ test.js
@@ -10,23 +10,40 @@
 var should = require('should');
 var handlebars = require('handlebars');
 var _ = require('lodash');
-var copyright = require('./');
+var copyrightHelper = require('./');
 
-var locals = {author: {name: 'Jon Schlinkert'}};
+var locals = {author: {name: 'Jon Schlinkert', url: 'https://github.com/jonschlinkert'}};
 
-describe('copyright', function () {
+describe('helper copyright', function () {
   it('should return a formatted copyright statement:', function () {
-    copyright(locals).should.eql('Copyright (c) 2014 Jon Schlinkert  ');
+    copyrightHelper(locals).should.eql('Copyright (c) 2014 Jon Schlinkert  ');
   });
 
   it('should work as a lodash helper:', function () {
-    _.template('<%= copyright({author: author}) %>', locals, {imports: {copyright: copyright}}).should.eql('Copyright (c) 2014 Jon Schlinkert  ');
+    _.template('<%= copyright({author: author}) %>', locals, {imports: {copyright: copyrightHelper}}).should.eql('Copyright (c) 2014 Jon Schlinkert  ');
   });
 
   it('should work as a handlebars helper:', function () {
-    handlebars.registerHelper('copyright', copyright);
+    handlebars.registerHelper('copyright', copyrightHelper);
 
     handlebars.compile('{{copyright this}}')(locals).should.eql('Copyright (c) 2014 Jon Schlinkert  ');
   });
 });
 
+describe('when `linkify` is `true`:', function () {
+  it('should return a formatted copyright statement:', function () {
+    locals = _.extend({linkify: true}, locals);
+    copyrightHelper(locals).should.eql('Copyright (c) 2014 [Jon Schlinkert](https://github.com/jonschlinkert)  ');
+  });
+
+  it('should work as a lodash helper:', function () {
+    _.template('<%= copyright({author: author, linkify: true}) %>', locals, {imports: {copyright: copyrightHelper}}).should.eql('Copyright (c) 2014 [Jon Schlinkert](https://github.com/jonschlinkert)  ');
+  });
+
+  it('should work as a handlebars helper:', function () {
+    locals = _.extend({linkify: true}, locals);
+    handlebars.registerHelper('copyright', copyrightHelper);
+
+    handlebars.compile('{{copyright this}}')(locals).should.eql('Copyright (c) 2014 [Jon Schlinkert](https://github.com/jonschlinkert)  ');
+  });
+});
